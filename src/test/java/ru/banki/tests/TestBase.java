bfe7dde28dc6f9da324755fa5e9a6880cd28ea72
alisitsky <alisitsky@gmail.com>
2023-12-20 15:54:38 +0100

    beforeAll config fix

--- src/test/java/ru/banki/tests/TestBase.java
+++ src/test/java/ru/banki/tests/TestBase.java
@@ -17,7 +17,7 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-//        Configuration.remote = "https://user1:1234@" + System.getProperty("remoteBrowserUrl", "selenoid.autotests.cloud") + "/wd/hub";
+        Configuration.remote = "https://user1:1234@" + System.getProperty("remoteBrowserUrl", "selenoid.autotests.cloud") + "/wd/hub";
         Configuration.baseUrl = System.getProperty("baseUrl","https://banki.ru");
 
         Configuration.browser = System.getProperty("browser", "chrome");
